5d04b8b70466c67f3838e6a8a2225a84aca79b1f
Gustavo Gervasio <gustavogervasio@gmail.com>
2021-01-03 13:29:06 -0300

    Create `HTTPClientResult` file on production

--- NetworkLayer/Client/HTTPClient.swift
+++ NetworkLayer/Client/HTTPClient.swift
@@ -1,21 +1,5 @@
 import Foundation
 
-internal enum HTTPClientResult: Equatable {
-    case failure(Error)
-    case success(Data, HTTPURLResponse)
-
-    static func == (lhs: HTTPClientResult, rhs: HTTPClientResult) -> Bool {
-        switch (lhs, rhs) {
-        case let (.failure(lhsError as NSError), .failure(rhsError as NSError)):
-            return lhsError.code == rhsError.code && lhsError.domain == rhsError.domain
-        case let (.success(lhsData, lhsResponse), .success(rhsData, rhsResponse)):
-            return lhsData == rhsData && lhsResponse == rhsResponse
-        default:
-            return false
-        }
-    }
-}
-
 internal protocol HTTPClient {
 
     func request(url: URL,
---
+++ NetworkLayer/Client/HTTPClientResult.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+internal enum HTTPClientResult: Equatable {
+    case failure(Error)
+    case success(Data, HTTPURLResponse)
+
+    static func == (lhs: HTTPClientResult, rhs: HTTPClientResult) -> Bool {
+        switch (lhs, rhs) {
+        case let (.failure(lhsError as NSError), .failure(rhsError as NSError)):
+            return lhsError.code == rhsError.code && lhsError.domain == rhsError.domain
+        case let (.success(lhsData, lhsResponse), .success(rhsData, rhsResponse)):
+            return lhsData == rhsData && lhsResponse == rhsResponse
+        default:
+            return false
+        }
+    }
+}
